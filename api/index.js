3af17aab317c6ea8cc4395cfc4e455e69cd3c74f
RachitReserk <trachit752@gmail.com>
2024-08-30 23:37:30 +0530

    Final Betav13

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ app.use(express.json())
   app.use(cookieParser())
 
   app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://classcache.netlify.app',
     credentials: true
   }));
 
